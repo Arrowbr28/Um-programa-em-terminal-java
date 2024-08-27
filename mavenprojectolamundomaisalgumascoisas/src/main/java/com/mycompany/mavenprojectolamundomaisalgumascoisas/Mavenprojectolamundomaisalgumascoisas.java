82bd634fa44bfe0624e71c8057092c95c743cc86
João Pedro Marchiori Santos <128313369+Arrowbr28@users.noreply.github.com>
2024-08-27 10:44:18 -0300

    Começando!!
    A longa jornada!

---
+++ mavenprojectolamundomaisalgumascoisas/src/main/java/com/mycompany/mavenprojectolamundomaisalgumascoisas/Mavenprojectolamundomaisalgumascoisas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenprojectolamundomaisalgumascoisas;
+
+/**
+ *
+ * @author joped
+ */
+public class Mavenprojectolamundomaisalgumascoisas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
